00f6b1c0d2eb17305205683926ae0a366bab9f64
navesm <stevennave@gmail.com>
2023-08-03 11:02:11 -0400

    made CarDealer model and get_dealership apis and views

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -44,6 +44,31 @@ class CarModel(models.Model):
         return f"{self.car_make} - {self.name}"
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, id, city, state, st, address, zip, lat, long, short_name, full_name):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer State
+        self.state = state
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
